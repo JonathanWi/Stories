4fe7082c14e5c5b0163b0b74995b00117e45ec6d
Jonathan Widawski <jonathan.widawski@gmail.com>
2015-09-15 01:56:50 +0200

    adding image & lightbox on image prompt

--- App/components/Comments/index.js
+++ App/components/Comments/index.js
@@ -9,6 +9,8 @@ var CommentCell = require('./CommentCell');
 
 var RedditApi = require('../../utils/RedditApi');
 
+var Lightbox = require('react-native-lightbox');
+var Icon = require('react-native-vector-icons/Ionicons');
 
 var {
   View,
@@ -16,6 +18,7 @@ var {
   Text,
   TouchableHighlight,
   ListView,
+  Image,
   ScrollView,
   AppStateIOS,
   ActivityIndicatorIOS
@@ -75,64 +78,58 @@ var Comments = React.createClass({
   },
 
   renderHeader: function() {
-    if(this.state.selftext !== '') {
-
-      // If image prompts, display image caroussel
-      if(this.state.type.name === 'Image Prompt') {
-        var geturl = new RegExp(
-          "((ftp|http|https|gopher|mailto|news|nntp|telnet|wais|file|prospero|aim|webcal):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))"
-         ,"g"
-        ),
-        urls = this.state.selftext.match(geturl);
-
-        return (
-          <View style={{backgroundColor:this.state.type.color}}>
-            <View style={styles.header}>
-              <Text style={styles.title}>
-                {this.state.title}
-              </Text>
-              <View style={{opacity:.5}}>
-                <Text style={styles.author}>
-                  — {this.state.author}
-                </Text>
-              </View>
-            </View>
-          </View>
-        );
-      }
-
-      // If writing prompt has selftext, display a carousel containing title & selftext
-      else {
-        return (
-          <View style={{backgroundColor:this.state.type.color}}>
-            <View style={styles.header}>
-              <Text style={styles.title}>
-                {this.state.title}
-              </Text>
-              <View style={{opacity:.5}}>
-                <Text style={styles.author}>
-                  — {this.state.author}
-                </Text>
-              </View>
-            </View>
-          </View>
-        );
-      }
-    }
-    return (
-      <View style={{backgroundColor:this.state.type.color}}>
-        <View style={styles.header}>
-          <Text style={styles.title}>
-            {this.state.title}
-          </Text>
-          <View style={{opacity:.5}}>
-            <Text style={styles.author}>
-              — {this.state.author}
+    // If image prompts, display image in header
+    if(this.state.type.name === 'Image Prompt') {
+      // Extract Image URL from selftext
+      var geturl = new RegExp(
+        "((ftp|http|https|gopher|mailto|news|nntp|telnet|wais|file|prospero|aim|webcal):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))"
+       ,"g"
+      ),
+      urls = this.state.selftext.match(geturl);
+      console.log(urls);
+      return (
+        <View style={{backgroundColor:this.state.type.color}}>
+          <View style={styles.header}>
+            <Text style={styles.title}>
+              {this.state.title}
             </Text>
+            <View style={{opacity:.5}}>
+              <Icon name="ios-search-strong" style={styles.magnify}>
+                <Text style={styles.instructions}>Tap to zoom</Text>
+              </Icon>
+            </View>
+            <Lightbox>
+              <Image
+                style={styles.cover}
+                resizeMode="cover"
+                source={{ uri: urls[0] }}
+              />
+            </Lightbox>
+            <View style={{opacity:.5}}>
+              <Text style={styles.author}>
+                — {this.state.author}
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
-    );
+      );
+    }
+    else {
+      return (
+        <View style={{backgroundColor:this.state.type.color}}>
+          <View style={styles.header}>
+            <Text style={styles.title}>
+              {this.state.title}
+            </Text>
+            <View style={{opacity:.5}}>
+              <Text style={styles.author}>
+                — {this.state.author}
+              </Text>
+            </View>
+          </View>
+        </View>
+      );
+    }
   },
 
   renderFooter: function() {
--- App/components/Comments/styles.js
+++ App/components/Comments/styles.js
@@ -22,6 +22,21 @@ var styles = StyleSheet.create({
   author: {
   	marginTop: 15,
   	color: '#000'
+  },
+  cover: {
+    height: 300,
+    borderColor: '#FFF',
+    borderWidth: 2,
+  },
+  magnify: {
+    fontSize: 12,
+    marginTop: 15,
+    marginBottom: 15,
+    color: '#FFF'
+  },
+  instructions: {
+    color: '#FFF',
+    marginLeft: 5
   }
 })
 
